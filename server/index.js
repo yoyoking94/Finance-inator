07f9afdea867194e571aa4fb6bda3ac7f93ac19e
yoyoking94 <yovish.moonesamy@gmail.com>
2024-04-15 20:25:30 +0200

    Error = data: undefined (row1.ts | line 12)

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ mongoose
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-        await mongoose.connection.db.dropDatabase();
-        KPI.insertMany(kpis);
+        /* await mongoose.connection.db.dropDatabase();
+        KPI.insertMany(kpis); */
     })
     .catch((error) => console.log(`${error} did not connect`));
