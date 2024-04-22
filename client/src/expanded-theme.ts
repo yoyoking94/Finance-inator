5d97a0642243d26137a2496cfa1746e78dff303f
yoyoking94 <yovish.moonesamy@gmail.com>
2024-04-22 19:20:37 +0200

    final version

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
-
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
     [key: number]: string;
