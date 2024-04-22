0a5fd347c65d66455b389b14def14762aef2f589
yoyoking94 <yovish.moonesamy@gmail.com>
2024-04-22 20:20:29 +0200

    final version

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -7,8 +7,8 @@ import PixIcon from "@mui/icons-material/Pix"
 
 
 const Navbar = () => {
-    const { palette } = useTheme()
-    const [selected, setSelected] = useState('dashboard')
+    const { palette } = useTheme();
+    const [selected, setSelected] = useState("dashboard");
 
     return (
         <FlexBetween mb=".25rem" p=".5rem 0rem" color={palette.grey[300]}>
@@ -18,14 +18,10 @@ const Navbar = () => {
             </FlexBetween>
             <FlexBetween gap="2rem">
                 <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-                    <Link to='/' onClick={() => setSelected("dashboard")} style={{
-                        color: selected === "dashboard" ? "inherit" : palette.grey[700], textDecoration: "inherit"
-                    }}>Dashboard</Link>
+                    <Link to='/' onClick={() => setSelected("dashboard")} style={{ color: selected === "dashboard" ? "inherit" : palette.grey[700], textDecoration: "inherit" }}>Dashboard</Link>
                 </Box>
                 <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-                    <Link to='/predictions' onClick={() => setSelected("predictions")} style={{
-                        color: selected === "predictions" ? "inherit" : palette.grey[700], textDecoration: "inherit"
-                    }}>Predictions</Link>
+                    <Link to='/predictions' onClick={() => setSelected("predictions")} style={{ color: selected === "predictions" ? "inherit" : palette.grey[700], textDecoration: "inherit" }}>Predictions</Link>
                 </Box>
             </FlexBetween>
         </FlexBetween >
